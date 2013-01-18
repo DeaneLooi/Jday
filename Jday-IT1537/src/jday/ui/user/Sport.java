ae64376592fdc900d8daa4f1e9c13d7e6ad05cc6
yuwen <yuwen@Yw-PC>
2013-01-18 17:16:39 +0800

    .

--- Jday-IT1537/src/jday/ui/user/Sport.java
+++ Jday-IT1537/src/jday/ui/user/Sport.java
@@ -59,27 +59,27 @@ public class Sport extends BackgroundPanel {
 		add(lblVolleyball);
 		
 		JLabel lblIcon = new JLabel("iconbadminton");
-		lblIcon.setIcon(new ImageIcon(Sport.class.getResource("/pic/badminton.png")));
+		lblIcon.setIcon(new ImageIcon(Sport.class.getResource("/images/badminton.png")));
 		lblIcon.setBounds(362, 46, 103, 101);
 		add(lblIcon);
 		
 		JLabel lblNewLabel_4 = new JLabel("iconBasketball");
-		lblNewLabel_4.setIcon(new ImageIcon(Sport.class.getResource("/pic/basketball.png")));
+		lblNewLabel_4.setIcon(new ImageIcon(Sport.class.getResource("/images/basketball.png")));
 		lblNewLabel_4.setBounds(81, 46, 103, 108);
 		add(lblNewLabel_4);
 		
 		JLabel lblIcontennis = new JLabel("iconTennis");
-		lblIcontennis.setIcon(new ImageIcon(Sport.class.getResource("/pic/tennis.png")));
+		lblIcontennis.setIcon(new ImageIcon(Sport.class.getResource("/images/tennis.png")));
 		lblIcontennis.setBounds(81, 196, 103, 102);
 		add(lblIcontennis);
 		
 		JLabel lblIconvolley = new JLabel("iconVolley");
-		lblIconvolley.setIcon(new ImageIcon(Sport.class.getResource("/pic/volleyball.png")));
+		lblIconvolley.setIcon(new ImageIcon(Sport.class.getResource("/images/volleyball.png")));
 		lblIconvolley.setBounds(362, 193, 103, 101);
 		add(lblIconvolley);
 		
 		JLabel lblIconbowling = new JLabel("iconBowling");
-		lblIconbowling.setIcon(new ImageIcon(Sport.class.getResource("/pic/bowling.png")));
+		lblIconbowling.setIcon(new ImageIcon(Sport.class.getResource("/images/bowling.png")));
 		lblIconbowling.setBounds(81, 324, 103, 108);
 		add(lblIconbowling);
 		
@@ -122,24 +122,6 @@ public class Sport extends BackgroundPanel {
 		btnNewButton_1.setBounds(194, 409, 117, 23);
 		add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("View Details");
-		btnNewButton_2.setBounds(new Rectangle(0, 0, 117, 0));
-		btnNewButton_2.setOpaque(false);
-		btnNewButton_2.setBounds(194, 367, 117, 23);
-		add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("View Details");
-		btnNewButton_3.setBounds(new Rectangle(0, 0, 117, 0));
-		btnNewButton_3.setOpaque(false);
-		btnNewButton_3.setBounds(194, 236, 117, 23);
-		add(btnNewButton_3);
-		
-		JButton btnViewDetails = new JButton("View Details");
-		btnViewDetails.setBounds(new Rectangle(0, 0, 117, 0));
-		btnViewDetails.setOpaque(false);
-		btnViewDetails.setBounds(490, 232, 117, 23);
-		add(btnViewDetails);
-		
 		JButton btnViewDetails_1 = new JButton("View Details");
 		btnViewDetails_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -151,20 +133,8 @@ public class Sport extends BackgroundPanel {
 			}
 		});
 		btnViewDetails_1.setOpaque(false);
-		btnViewDetails_1.setBounds(194, 85, 117, 23);
+		btnViewDetails_1.setBounds(490, 455, 117, 23);
 		add(btnViewDetails_1);
-		
-		JButton btnViewDetails_2 = new JButton("View Details");
-		btnViewDetails_2.setBounds(new Rectangle(0, 0, 117, 0));
-		btnViewDetails_2.setOpaque(false);
-		btnViewDetails_2.setBounds(490, 81, 117, 23);
-		add(btnViewDetails_2);
-		
-		JButton btnNewButton_4 = new JButton("View Details");
-		btnNewButton_4.setBounds(new Rectangle(0, 0, 117, 0));
-		btnNewButton_4.setOpaque(false);
-		btnNewButton_4.setBounds(490, 367, 117, 23);
-		add(btnNewButton_4);
 		
 		JButton btnBookNow_1 = new JButton("Book Now");
 		btnBookNow_1.setBounds(new Rectangle(0, 0, 117, 0));
@@ -177,7 +147,7 @@ public class Sport extends BackgroundPanel {
 		add(lblTableTennis);
 		
 		JLabel lblNewLabel_5 = new JLabel("iconTTennis");
-		lblNewLabel_5.setIcon(new ImageIcon(Sport.class.getResource("/pic/ttennis.png")));
+		lblNewLabel_5.setIcon(new ImageIcon(Sport.class.getResource("/images/ttennis.png")));
 		lblNewLabel_5.setBounds(362, 325, 103, 106);
 		add(lblNewLabel_5);
 		
